74c2dc2bb13d1b4305373c94972214feb367af7e
Fernando Barroso <fernandobarroso@suaquadra.com.br>
2025-08-07 00:00:55 -0700

    change credentials for the s3 - now using the envs

--- udagram-api-feed/src/aws.ts
+++ udagram-api-feed/src/aws.ts
@@ -1,27 +1,26 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import { fromIni } from "@aws-sdk/credential-providers";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
-import { config } from './config/config';
+import { config } from "./config/config";
 
-// Configure AWS
-const credentials = fromIni({ profile: config.aws_profile });
-
-// Create S3 client
+// Create S3 client with environment variables from config
 export const s3Client = new S3Client({
   region: config.aws_region || "us-east-1",
-  credentials: credentials,
+  credentials: {
+    accessKeyId: config.aws_access_key_id || "",
+    secretAccessKey: config.aws_secret_access_key || "",
+  },
 });
 
 // Generates an AWS signed URL for retrieving objects
 export async function getGetSignedUrl(key: string): Promise<string> {
   const command = new GetObjectCommand({
     Bucket: config.aws_media_bucket,
-    Key: key
+    Key: key,
   });
 
   const signedUrl = await getSignedUrl(s3Client, command, {
-    expiresIn: 60 * 5 // 5 minutes
+    expiresIn: 60 * 5, // 5 minutes
   });
 
   return signedUrl;
@@ -31,11 +30,11 @@ export async function getGetSignedUrl(key: string): Promise<string> {
 export async function getPutSignedUrl(key: string): Promise<string> {
   const command = new PutObjectCommand({
     Bucket: config.aws_media_bucket,
-    Key: key
+    Key: key,
   });
 
   const signedUrl = await getSignedUrl(s3Client, command, {
-    expiresIn: 60 * 5 // 5 minutes
+    expiresIn: 60 * 5, // 5 minutes
   });
 
   return signedUrl;
--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -1,13 +1,12 @@
-import cors from 'cors';
-import express from 'express';
-import {sequelize} from './sequelize';
+import cors from "cors";
+import express from "express";
+import { sequelize } from "./sequelize";
 
-import {IndexRouter} from './controllers/v0/index.router';
+import { IndexRouter } from "./controllers/v0/index.router";
 
-import bodyParser from 'body-parser';
-import {config} from './config/config';
-import {V0_USER_MODELS} from './controllers/v0/model.index';
-
+import bodyParser from "body-parser";
+import { config } from "./config/config";
+import { V0_USER_MODELS } from "./controllers/v0/model.index";
 
 (async () => {
   await sequelize.addModels(V0_USER_MODELS);
@@ -19,29 +18,33 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
   const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
-  
-  app.use(cors({
-    allowedHeaders: [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization',
-    ],
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    origin: '*',
-  }));
 
-  app.use('/api/v0/', IndexRouter);
+  app.use(
+    cors({
+      allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "X-Access-Token",
+        "Authorization",
+      ],
+      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+      preflightContinue: true,
+      origin: "*",
+    })
+  );
+
+  app.use("/api/v0/", IndexRouter);
 
   // Root URI call
-  app.get( '/', async ( req, res ) => {
-    res.send( '/api/v0/' );
-  } );
-
+  app.get("/", async (req, res) => {
+    res.send("/api/v0/");
+  });
 
   // Start the Server
-  app.listen( port, () => {
-    console.log( `server running ${config.url}` );
-    console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running ${config.url}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
